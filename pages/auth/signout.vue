969abf9c5d1bf05c39f9c7ae1db1e50463f3286e
Prakash Kumar <prakash20kumar2000@gmail.com>
2022-03-03 22:22:30 +0530

    initial commit

---
+++ pages/auth/signout.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Sign Out</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+ <h1>Trello</h1>
+</template>
+
+<script>
+export default {
+ 
+}
+</script>
